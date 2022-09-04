5d054a528bc22f8883981d132e3a1cda45c8e523
Tyler Weldon <tylerweldon94@gmail.com>
2022-09-04 18:52:13 -0400

    start dl script

--- docker/downloader/src/astra-dl.sh
+++ docker/downloader/src/astra-dl.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-steamcmd +login $STEAM_USER $STEAM_PASS +download_depot $KSP_STEAM_APP_ID $KSP_STEAM_DEPOT_ID $KSP_STEAM_MANIFEST_ID +quit
+steamcmd +force_install_dir /mnt/astra +login $STEAM_USER $STEAM_PASS +download_depot $KSP_STEAM_APP_ID $KSP_STEAM_DEPOT_ID $KSP_STEAM_MANIFEST_ID +quit
+mv /home/steam/steamcmd/linux32/steamapps /mnt/astra
